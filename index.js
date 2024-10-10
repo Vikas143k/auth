2180d66678a430cc6d864b660b24809505aef663
Vikas Yadav <vikyadav20000@gmail.com>
2024-10-11 03:57:20 +0530

    included logout

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
-const user=require("./models/user");
-
+const User=require("./models/user");
+const Bcrypt=require("Bcrypt");
+const session=require("express-session");
 app.set("view engine",'ejs');
 app.set("views",'views');
 
-
+app.use(session({
+    secret:"nosecret",
+    resave: true,
+    saveUninitialized: true
+}));
 mongoose.connect('mongodb://127.0.0.1:27017/auth')
 .then(()=>{
     console.log("mongoose CONNECTION ESTABLISHED")
@@ -20,10 +25,43 @@ app.get("/register",(req,res)=>{
     res.render("register")
 })
 app.get("/secret",(req,res)=>{
-    res.send("This is a secret")
+    if(!req.session.userId){
+        return res.redirect("/login");
+    }
+    res.render("secret");
 })
-app.post("/register",(req,res)=>{
-        res.send(req.body);
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.post("/login",async(req,res)=>{
+    const {username,password}=req.body;
+    const valid=await User.findAndValidate(username,password);
+    if(valid){
+        req.session.userId=valid._id;
+        return res.redirect("secret");
+    }else{
+
+        res.redirect("login");
+    }
+    
+})
+app.post("/register",async(req,res)=>{
+    const {username,password}=req.body;
+    const hash =await Bcrypt.hash(password,12);
+    const user=new User({
+        username,
+        password:hash
+    });
+    await user.save();
+    req.session.userId=user._id;
+        res.render("secret");
+})
+app.get('/',(req,res)=>{
+    res.send("HOME");
+})
+app.post("/logout",(req,res)=>{
+    req.session.destroy();
+    res.redirect("/login");
 })
 app.listen(3000,()=>{
     console.log("App is working");
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const Bcrypt=require("Bcrypt");
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -9,4 +10,10 @@ const userSchema=new mongoose.Schema({
         required:[true,"password cannot be blank!"]
     }
 })
+userSchema.statics.findAndValidate=async function(username,password){
+    const foundUser=await this.findOne({username});
+    const isValid=await Bcrypt.compare(password,foundUser.password);
+    return isValid?foundUser:false;
+}
+
 module.exports=mongoose.model('User',userSchema)
